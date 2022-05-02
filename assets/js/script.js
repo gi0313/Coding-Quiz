89e257fb06c30588f816ddf6f1817cf18b0ae443
Giovanni Claro <gio.claro316@gmail.com>
2022-05-01 22:01:26 -0400

    reworked javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,50 +2,82 @@
 var questions = [
     {
     question: "Commonly used data values do not include", 
-    answers: ["strings", "booleans", "alerts", "numbers"],
+    answer1: "strings",
+    answer2: "booleans",
+    answer3: "alerts",
+    answer4: "numbers",
     correctAnswer: "2"
     },
     {
     question: "The condition in an if/else statement is enclosed with _____.", 
-    answers: ["quotes", "curly brackets", "parenthesis", "square brackets"],
-    correctAnswer: "1"
-    },
-    {
-    question: "Arrays in JavaScript can be used to store ____.", 
-    answers: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-    correctAnswer: "3"
-    },
-    {
-    question: "String values must be enclosed within ____ when being assigned to variables.", 
-    answers: ["commas", "curly brackets", "quotes", "parenthesis"],
+    answer1: "quotes",
+    answer2: "curly brackets",
+    answer3: "parenthesis",
+    answer4: "square brackets",
     correctAnswer: "2"
     },
     {
-    question: "A very useful tool used during development and debugging for printing content to the debugger is:", 
-    answers: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+    question: "Arrays in JavaScript can be used to store ____.", 
+    answer1: "numbers and strings",
+    answer2: "other arrays",
+    answer3: "booleans",
+    answer4: "all of the above",
+    correctAnswer: "4"
+    },
+    {
+    question: "String values must be enclosed within ____ when being assigned to variables.", 
+    answer1: "commas",
+    answer2: "curly brackets",
+    answer3: "quotes",
+    answer4: "parenthesis",
     correctAnswer: "3"
     },
+    {
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:", 
+    answer1: "JavaScript",
+    answer2: "terminal/bash",
+    answer3: "for loops",
+    answer4: "console.log",
+    correctAnswer: "4"
+    },
 ]
- var time = 60;
- var startButton = document.getElementById("start-btn");
- var questionBoxEl = document.getElementById("question-box");
+var time = 60;
+var startButton = document.getElementById("start-btn");
+var questionBoxEl = document.getElementById("question-box");
+var timerEl = document.getElementById("timer");
+var answer1 = document.getElementById("1");
+var answer2 = document.getElementById("2");
+var answer3 = document.getElementById("3");
+var answer4 = document.getElementById("4");
+
+var lastQuestionIndex = question.length -1;
+var runningQuestionIndex = 0;
 
 //quiz start
 var startGame = function() {
     startButton.remove();
-    console.log("started");
 
-    nextQuestion();
+    renderQuestion();
 }
+//timer rundown
+function countdown() {
+    time--;
+    timerEl.textContent = "Time: " + timeLeft;
+    if (timeLeft <= 0) {
+    saveScore();
+    }
+   }
 
-//next question
-var nextQuestion = function() {
-    var questionBox = document.createElement("div");
+//question rendering
+var renderQuestion = function() {
+    var questionBox = questions[runningQuestionIndex];
 
-    questionBox.innerHTML = questions[0].question;
+    questionBox.innerHTML= "<p>" + questionBox.question+ "<p>";
+    answer1.innerHTML= questionBox.answer1;
+    answer2.innerHTML= questionBox.answer2;
+    answer3.innerHTML= questionBox.answer3
+    answer4.innerHTML= questionBox.answer4
 }
-
-//actual question
 
 
 startButton.addEventListener("click", startGame);
